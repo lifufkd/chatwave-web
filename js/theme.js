2802b58e2119d9c6dbfeb8b07f7ff9da81ef6ea5
lilfufkd <dmsemen45@gmail.com>
2025-04-16 13:53:50 +0300

    Add custom font. Add main page

--- js/theme.js
+++ js/theme.js
@@ -6,12 +6,30 @@ const themeIcon = document.getElementById('theme-icon');
 function setTheme(theme) {
   themeLink.href = theme === 'dark' ? '../styles/themes/dark.css' : '../styles/themes/light.css';
   logo.src = theme === 'dark' ? '../assests/logo-dark.png' : '../assests/logo-light.png';
-  themeIcon.className = theme === 'dark' ? 'bi bi-sun-fill' : 'bi bi-moon-fill';
+  themeIcon.className = theme === 'dark' ? 'bi bi-sun-fill text-white' : 'bi bi-moon-fill text-dark';
   localStorage.setItem('theme', theme);
 }
 
+function updateAllIcons(theme) {
+  const icons = document.querySelectorAll('.bi'); // выбираем все иконки Bootstrap Icons
+
+  icons.forEach(icon => {
+    // Удалим старые классы цвета
+    icon.classList.remove('text-dark', 'text-white');
+
+    // Добавим нужный
+    if (theme === 'dark') {
+      icon.classList.add('text-white');
+    } else {
+      icon.classList.add('text-dark');
+    }
+  });
+}
+
+
 function toggleTheme() {
   const current = localStorage.getItem('theme') === 'dark' ? 'light' : 'dark';
+  updateAllIcons(current);
   setTheme(current);
 }
 
@@ -23,8 +41,10 @@ themeToggle.addEventListener('click', toggleTheme);
   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
   if (savedTheme) {
+    updateAllIcons(savedTheme);
     setTheme(savedTheme);
   } else {
+    updateAllIcons(prefersDark ? 'dark' : 'light');
     setTheme(prefersDark ? 'dark' : 'light');
   }
 })();
