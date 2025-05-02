817e9e26424ec18f1b2c53b995ad6ea39188ab98
SBR <dmsemen45@gmail.com>
2025-05-02 14:38:55 +0300

    Add emoji selection support

--- js/main/emoji.js
+++ js/main/emoji.js
@@ -1,26 +1,22 @@
+const { createPopup } = window.picmoPopup;
+
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('emoji-button');
+    const chat_container = document.getElementById('emoji-container');
     const input = document.getElementById('chat-message-input');
 
-    const picker = new EmojiButton({
+    const picker = createPopup({}, {
+      referenceElement: button,
+      triggerElement: button,
       position: 'top-start',
-      theme: 'light',
-      zIndex: 1050, // выше Bootstrap modal
-      autoHide: true,
-      rootElement: document.querySelector('#chat-footer'),
+      theme: 'light'
     });
-
-    picker.on('emoji', emoji => {
-      const start = input.selectionStart;
-      const end = input.selectionEnd;
-      const text = input.value;
-      input.value = text.slice(0, start) + emoji + text.slice(end);
-      input.setSelectionRange(start + emoji.length, start + emoji.length);
-      input.focus();
-    });
-
+  
     button.addEventListener('click', () => {
-      console.log(2222);
-      picker.togglePicker(button);
+      picker.toggle();
+    });
+  
+    picker.addEventListener('emoji:select', (selection) => {
+      input.value += selection.emoji;
     });
   });
