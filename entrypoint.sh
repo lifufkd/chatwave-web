da8bea29d81e1e21a581b88c37518e7ed5076480
SBR <dmsemen45@gmail.com>
2025-05-04 19:31:26 +0300

    Add https support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-API_URL="http://${API_URL:-127.0.0.1}"
+API_URL="${API_URL:-http://127.0.0.1}"
 API_PORT="${API_PORT:-8000}"
 API_BASE_URL="${API_URL}:${API_PORT}"
 JWT_ACCESS_TOKEN_EXPIRES="${JWT_ACCESS_TOKEN_EXPIRES:-1209500}"
@@ -13,5 +13,30 @@ export const API_TOKEN_LIFESPAN = ${JWT_ACCESS_TOKEN_EXPIRES};
 export const LONG_POLLING_DELAY = ${LONG_POLLING_DELAY};
 export const DEFAULT_MESSAGES_QUANTITY = ${DEFAULT_MESSAGES_QUANTITY};
 EOF
+
+if [ -n "$SSL_CERT_PATH" ] && [ -n "$SSL_CERT_KEY" ]; then
+  HTTPS_BLOCK=$(cat <<EOF
+    server {
+        listen 443 ssl;
+        server_name localhost;
+
+        ssl_certificate     $SSL_CERT_PATH;
+        ssl_certificate_key $SSL_CERT_KEY;
+
+        root /usr/share/nginx/html;
+        index index.html;
+
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
+    }
+EOF
+)
+else
+  HTTPS_BLOCK=""
+fi
+
+envsubst '{{HTTPS_BLOCK}}' < /etc/nginx/nginx.conf.template | \
+  sed "s|{{HTTPS_BLOCK}}|$HTTPS_BLOCK|" > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
